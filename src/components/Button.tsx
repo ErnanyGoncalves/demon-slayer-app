e3c2e9bb7f6326c7205884e65a1e20448240c73a
Ernany <ernany.goncalves@sensedia.com>
2022-03-17 09:13:04 -0300

    alteração na pasta resources para assets; adição de todas as fontes; criação das variaveis de cores

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,22 +4,22 @@ const StyledButton = styled.div`
         display:inline-flex;
         justify-content:space-between;
         align-items:center;
-        border: 2px solid #010000; 
+        border: 2px solid $black; 
         box-sizing:border-box;
         gap: 1rem;
         padding: 1rem;
         font-size: 1.5rem;
-        background-color: #FFFFFF;
+        background-color: $white;
         cursor: pointer;    
         
         :hover{
-            outline: 2px solid #EE1C25;
+            outline: 2px solid $red;
         }
 
         div{
             height: 2rem;
             width: 2rem;
-            background: url("src/resources/icons/add.svg");
+            background: url("src/assets/icons/add.svg");
             background-repeat:no-repeat;
             background-size: 100% 100%;
         }
